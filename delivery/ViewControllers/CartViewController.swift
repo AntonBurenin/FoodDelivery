0a7ee1020a774b3c7dce0b17f2626dc7b11f3f75
AntonBu <burkinband@mail.ru>
2020-06-14 22:35:47 +0300

    Revert "Merge branch 'beta' into dishForAnton"
    This reverts commit 8a8604664b12fe5f20407f63ca9fa7a36b3ccec8, reversing
    changes made to ceccc8b66bb32fc51332e0ba9a8c3f97dac385ff.

--- delivery/ViewControllers/CartViewController.swift
+++ delivery/ViewControllers/CartViewController.swift
@@ -8,77 +8,32 @@
 
 import UIKit
 
-class CartViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CartTableViewCellDelegate {
-    
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var confirmButton: UIButton!
-    @IBOutlet weak var sumLabel: UILabel!
-    
-    var dishes = Cart.shared.getDishes()
-
-    @IBOutlet var viewResult: UIView!
+class CartViewController: UIViewController, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        viewResult.customViewDesing()
-        confirmButton.customViewDesing()
-        
-        navigationItem.title = "Ваш заказ"
-        sumLabel.text = constructTextForSumLabel()
+
+        // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dishes.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath) as! CartTableViewCell
-        
-        let dish = dishes[indexPath.row].0
-        
-        cell.nameLabel.text = dish.name
-        cell.priceLabel.text = String(format: "%.2f", dish.price)
-        
-        cell.stepperCustom.minimumValue = 0
-        cell.stepperCustom.maximumValue = 9
-        cell.stepperCustom.value = Double(dishes[indexPath.row].1)
-        cell.stepperCustom.tag =  indexPath.row
-        
-        cell.countLabel.text = cell.constructTextForCountLabel()
-        cell.photoImageView.image = UIImage(named: dish.photo)
-
-        cell.delegate = self
-        
-        cell.contentViewPhoto.customViewDesing()
-        cell.photoView.customViewDesing()
-        cell.photoImageView.customViewDesing()
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath)
+        cell.textLabel?.text = "Dish \(indexPath.row)"
         return cell
     }
-    
-    func didTappedStepper(cell: CartTableViewCell) {
-        if let indexPath = tableView.indexPath(for: cell) {
-            cell.countLabel.text = cell.constructTextForCountLabel()
-            Cart.shared.dishes[dishes[indexPath.row].0] = Int(cell.stepperCustom.value) != 0 ? Int(cell.stepperCustom.value) : nil
-            sumLabel.text = constructTextForSumLabel()
-            if cell.stepperCustom.value == 0 {
-                dishes = Cart.shared.getDishes()
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            } else {
-                dishes = Cart.shared.getDishes()
-                tableView.reloadData()
-                print(sumLabel.text)
-            }
-        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    private func constructTextForSumLabel() -> String {
-        return String(format: "%.2f", Cart.shared.getSum()) + "руб"
-    }
+    */
 
 }
