21ab157347dc131b872f74dd20da8967a117d539
AntonBu <burkinband@mail.ru>
2020-06-14 22:39:10 +0300

    Revert "Revert "Revert "Merge branch 'beta' into dishForAnton"""
    This reverts commit 3b5a14fd2451b0d1e34866a85ce4a90ade35affe.

--- delivery/ViewControllers/CategoryTableViewController.swift
+++ delivery/ViewControllers/CategoryTableViewController.swift
@@ -14,27 +14,25 @@ class CategoryTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //не забыть удалить, если не изменится
+
     }
 
     // MARK: - Table view data source
 
+
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listCategory.count // можно не писать return
+       
+        return listCategory.count // можно не писать return 
     }
 
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "category", for: indexPath) as! CustomTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "category", for: indexPath)
 
-        cell.catigoriesLabel.text = listCategory[indexPath.row].name.rawValue.capitalized
+        cell.textLabel?.text = listCategory[indexPath.row].name.rawValue
         
-        cell.customImageView.image =  UIImage(named: listCategory[indexPath.row].name.rawValue)
-        cell.customImageView.layer.cornerRadius = cell.customImageView.frame.height / 4
-        cell.customImageView.clipsToBounds = true
-        
-        cell.customView.customViewDesing()
-        cell.contentCustomView.customViewDesing()
-        
+
         return cell
     }
     
@@ -48,17 +46,3 @@ class CategoryTableViewController: UITableViewController {
     }
 
 }
-
-// MARK:- DesingSettings for Elements
-
-extension UIView {
-    func customViewDesing() {
-        self.layer.cornerRadius = self.frame.height / 4
-        self.layer.shadowOpacity = 7
-        self.layer.shadowRadius = 3
-        //self.layer.shadowOffset = .zero
-        self.layer.shadowOffset = CGSize(width: 0.8, height: 1.1)
-    }
-}
-
-
